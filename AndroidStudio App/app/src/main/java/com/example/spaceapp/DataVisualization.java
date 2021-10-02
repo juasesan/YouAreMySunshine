067a0ce941e14088bb8cf785ddc4327d75bf2c40
Juan Sánchez <juasesan@espol.edu.ec>
2021-10-02 13:48:45 -0500

    Add DataVisualization Class

---
+++ AndroidStudio App/app/src/main/java/com/example/spaceapp/DataVisualization.java
@@ -0,0 +1,10 @@
+package com.example.spaceapp;
+
+import java.util.HashMap;
+
+public class DataVisualization {
+    public HashMap<String, Integer> data = new HashMap<String, Integer>();
+    data.put("20210102": 1.55);
+    data.put("20210103": 1.7);
+    data.put("20210104" 2.09);
+}
